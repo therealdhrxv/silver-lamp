34bd7826e2ed8885659dcd1e92600ce91fb43179
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2024-06-15 20:00:01 +0530

    prod 🚀

--- app/page.js
+++ app/page.js
@@ -1,7 +1,53 @@
+"use client";
+
+import { useState, useRef } from "react";
+
 export default function Home() {
+
+	const [otp, setOtp] = useState(["", "", "", ""]);
+
+	const inputRef = useRef([]);
+
+	const handleChange = (index, value) => {
+		const newOtp = [...otp];
+		newOtp[index] = value;
+		if (value && index < otp.length - 1) inputRef.current[index + 1].focus();
+		setOtp(newOtp);
+	}
+
+	const handleKeyDown = (index, e) => {
+		if (e.key === "Backspace" && index > 0 && !otp[index]) {
+			const newOtp = [...otp];
+			newOtp[index - 1] = "";
+			inputRef.current[index - 1].focus();
+			setOtp(newOtp);
+		}
+	}
+
 	return (
 		<>
-			Lol
+			<div className="app">
+
+				<h2> OTP VERIFICATION </h2>
+				<p> Enter the four digit otp that you've received </p>
+
+				<div className="otp-container">
+					{otp.map((digit, index) => (
+						<input
+							key={index}
+							className="otp-input"
+							type="text"
+							maxLength={1}
+							value={digit}
+							autoFocus={index === 0}
+							ref={(ref) => (inputRef.current[index] = ref)}
+							onChange={(e) => handleChange(index, e.target.value)}
+							onKeyDown={(e) => handleKeyDown(index, e)}
+						/>
+					))}
+				</div>
+
+			</div>
 		</>
 	);
 }
